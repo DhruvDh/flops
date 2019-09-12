446f7d7886100d5b4c6e0374d5fb57b90a10b99c
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2019-09-12 14:05:46 -0400

    brute forcing iops

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,18 @@ use rayon;
 use packed_simd::{i32x8, f32x8};
 use std::time::Instant;
 
-/// returns an array with 8 floats that will use SIMD instructions for operations
-fn new_simd_float() -> f32x8 {
-    let mut rng = rand::thread_rng();
-    let random_floats: Vec<f32> = (0..8).map(|_| rng.gen()).collect();
-    
-    f32x8::from_slice_unaligned(&random_floats[0..8])
-}
-
-/// returns an array with 8 ints that will use SIMD instructions for operations
-fn new_simd_int() -> i32x8 {
-    let mut rng = rand::thread_rng();
-    let random_ints: Vec<i32> = (0..8).map(|_| rng.gen()).collect();
-
-    i32x8::from_slice_unaligned(&random_ints[0..8])
-}
-
-macro_rules! create_variables {
-    ($($x:ident,)+, $y:ident) => {
+macro_rules! init_variables {
+    ([$($x:ident),+], $rng:ident) => {
         $(
-            let mut $x = $y();
+            let mut $x = {
+                let random_ints: Vec<i32> = (0..8).map(|_| $rng.gen()).collect();
+                i32x8::from_slice_unaligned(&random_ints[0..8])
+            };
+        )+
+    };
+    (($($x:ident),+), $rng:ident) => {
+        $(
+            let mut $x: i32 = $rng.gen();
         )+
     };
 }
@@ -35,25 +27,170 @@ macro_rules! debug_them {
     }
 }
 
-fn do_math() {
-    create_variables!(a, b, c, x, y, i,, new_simd_int);
-    let mut rng = rand::thread_rng();
-    let mut z: i32 = rng.gen();
-    let j: i32 = rng.gen();
+macro_rules! math {
+    ($a:ident, $b:ident, $c:ident, $x:ident, $y:ident, $i:ident, $z:ident, $j:ident) => {
+        $a = $a * $b;
+        $c = $c + $x;
+        $y = $y - $i;
+        $z = $z.wrapping_add($j);
+    };
+}
 
-    // macro that expands to let a = new_simd_float(); for each variable a, b, c...
-    // basically initializes each variable to an array of random 8 32-bit floats that use SIMD operations 
+fn do_math_1() {
+    let mut rng = rand::thread_rng();
+
+    init_variables!([a, b, c, x, y, i], rng);
+    init_variables!([Aa, Ab, Ac, Ax, Ay, Ai], rng);
+    init_variables!([Ba, Bb, Bc, Bx, By, Bi], rng);
+    init_variables!([Ca, Cb, Cc, Cx, Cy, Ci], rng);
+    init_variables!([Da, Db, Dc, Dx, Dy, Di], rng);
+    // macro that expands to code that initializes each variable to an array of random 8 32-bit floats that use SIMD operations 
+
+    init_variables!((z, j, Az, Aj, Bz, Bj), rng);
+    init_variables!((Cz, Cj, Dz, Dj), rng);
+    // same marco also initialized to a random i32 if variables are inside () instead of []
+
     
-    for _ in 0..(1e10 as i64) {
-        a = a * b;
-        c = c + x;
-        y = y - i;
-        z = z.wrapping_add(j);
+    for _ in 0..(1e9 as i64) {
+        math!(a, b, c, x, y, i, z, j);
+        math!(Aa, Ab, Ac, Ax, Ay, Ai, Az, Aj);
+        math!(Ba, Bb, Bc, Bx, By, Bi, Bz, Bj);
+        math!(Ca, Cb, Cc, Cx, Cy, Ci, Cz, Cj);
+        math!(Da, Db, Dc, Dx, Dy, Di, Dz, Dj);
     }
     
-    debug_them!(a, c, y);
+    debug_them!(a, c, y, z);
+    // debug_them!(Aa, Ac, Ay, Az);
+    // debug_them!(Ba, Bc, By, Bz);
+    // debug_them!(Ca, Cc, Cy, Cz);
+    // debug_them!(Da, Dc, Dy, Dz);
     // macro that prints each variable to std_err to ensure compiler doesn't "optimize" away the computation
 }
+
+fn do_math_2() {
+    let mut rng = rand::thread_rng();
+
+    init_variables!([a, b, c, x, y, i], rng);
+    init_variables!([Aa, Ab, Ac, Ax, Ay, Ai], rng);
+    init_variables!([Ba, Bb, Bc, Bx, By, Bi], rng);
+    init_variables!([Ca, Cb, Cc, Cx, Cy, Ci], rng);
+    init_variables!([Da, Db, Dc, Dx, Dy, Di], rng);
+    // macro that expands to code that initializes each variable to an array of random 8 32-bit floats that use SIMD operations 
+
+    init_variables!((z, j, Az, Aj, Bz, Bj), rng);
+    init_variables!((Cz, Cj, Dz, Dj), rng);
+    // same marco also initialized to a random i32 if variables are inside () instead of []
+
+    
+    for _ in 0..(1e9 as i64) {
+        math!(a, b, c, x, y, i, z, j);
+        math!(Aa, Ab, Ac, Ax, Ay, Ai, Az, Aj);
+        math!(Ba, Bb, Bc, Bx, By, Bi, Bz, Bj);
+        math!(Ca, Cb, Cc, Cx, Cy, Ci, Cz, Cj);
+        math!(Da, Db, Dc, Dx, Dy, Di, Dz, Dj);
+    }
+    
+    debug_them!(a, c, y, z);
+    debug_them!(Aa, Ac, Ay, Az);
+    // debug_them!(Ba, Bc, By, Bz);
+    // debug_them!(Ca, Cc, Cy, Cz);
+    // debug_them!(Da, Dc, Dy, Dz);
+    // macro that prints each variable to std_err to ensure compiler doesn't "optimize" away the computation
+}
+
+fn do_math_3() {
+    let mut rng = rand::thread_rng();
+
+    init_variables!([a, b, c, x, y, i], rng);
+    init_variables!([Aa, Ab, Ac, Ax, Ay, Ai], rng);
+    init_variables!([Ba, Bb, Bc, Bx, By, Bi], rng);
+    init_variables!([Ca, Cb, Cc, Cx, Cy, Ci], rng);
+    init_variables!([Da, Db, Dc, Dx, Dy, Di], rng);
+    // macro that expands to code that initializes each variable to an array of random 8 32-bit floats that use SIMD operations 
+
+    init_variables!((z, j, Az, Aj, Bz, Bj), rng);
+    init_variables!((Cz, Cj, Dz, Dj), rng);
+    // same marco also initialized to a random i32 if variables are inside () instead of []
+
+    
+    for _ in 0..(1e9 as i64) {
+        math!(a, b, c, x, y, i, z, j);
+        math!(Aa, Ab, Ac, Ax, Ay, Ai, Az, Aj);
+        math!(Ba, Bb, Bc, Bx, By, Bi, Bz, Bj);
+        math!(Ca, Cb, Cc, Cx, Cy, Ci, Cz, Cj);
+        math!(Da, Db, Dc, Dx, Dy, Di, Dz, Dj);
+    }
+    
+    debug_them!(a, c, y, z);
+    debug_them!(Aa, Ac, Ay, Az);
+    debug_them!(Ba, Bc, By, Bz);
+    // debug_them!(Ca, Cc, Cy, Cz);
+    // debug_them!(Da, Dc, Dy, Dz);
+    // macro that prints each variable to std_err to ensure compiler doesn't "optimize" away the computation
+}
+
+fn do_math_4() {
+    let mut rng = rand::thread_rng();
+
+    init_variables!([a, b, c, x, y, i], rng);
+    init_variables!([Aa, Ab, Ac, Ax, Ay, Ai], rng);
+    init_variables!([Ba, Bb, Bc, Bx, By, Bi], rng);
+    init_variables!([Ca, Cb, Cc, Cx, Cy, Ci], rng);
+    init_variables!([Da, Db, Dc, Dx, Dy, Di], rng);
+    // macro that expands to code that initializes each variable to an array of random 8 32-bit floats that use SIMD operations 
+
+    init_variables!((z, j, Az, Aj, Bz, Bj), rng);
+    init_variables!((Cz, Cj, Dz, Dj), rng);
+    // same marco also initialized to a random i32 if variables are inside () instead of []
+
+    
+    for _ in 0..(1e9 as i64) {
+        math!(a, b, c, x, y, i, z, j);
+        math!(Aa, Ab, Ac, Ax, Ay, Ai, Az, Aj);
+        math!(Ba, Bb, Bc, Bx, By, Bi, Bz, Bj);
+        math!(Ca, Cb, Cc, Cx, Cy, Ci, Cz, Cj);
+        math!(Da, Db, Dc, Dx, Dy, Di, Dz, Dj);
+    }
+    
+    debug_them!(a, c, y, z);
+    debug_them!(Aa, Ac, Ay, Az);
+    debug_them!(Ba, Bc, By, Bz);
+    debug_them!(Ca, Cc, Cy, Cz);
+    // debug_them!(Da, Dc, Dy, Dz);
+    // macro that prints each variable to std_err to ensure compiler doesn't "optimize" away the computation
+}
+
+fn do_math_5() {
+    let mut rng = rand::thread_rng();
+
+    init_variables!([a, b, c, x, y, i], rng);
+    init_variables!([Aa, Ab, Ac, Ax, Ay, Ai], rng);
+    init_variables!([Ba, Bb, Bc, Bx, By, Bi], rng);
+    init_variables!([Ca, Cb, Cc, Cx, Cy, Ci], rng);
+    init_variables!([Da, Db, Dc, Dx, Dy, Di], rng);
+    // macro that expands to code that initializes each variable to an array of random 8 32-bit floats that use SIMD operations 
+
+    init_variables!((z, j, Az, Aj, Bz, Bj), rng);
+    init_variables!((Cz, Cj, Dz, Dj), rng);
+    // same marco also initialized to a random i32 if variables are inside () instead of []
+
+    
+    for _ in 0..(1e9 as i64) {
+        math!(a, b, c, x, y, i, z, j);
+        math!(Aa, Ab, Ac, Ax, Ay, Ai, Az, Aj);
+        math!(Ba, Bb, Bc, Bx, By, Bi, Bz, Bj);
+        math!(Ca, Cb, Cc, Cx, Cy, Ci, Cz, Cj);
+        math!(Da, Db, Dc, Dx, Dy, Di, Dz, Dj);
+    }
+    
+    debug_them!(a, c, y, z);
+    debug_them!(Aa, Ac, Ay, Az);
+    debug_them!(Ba, Bc, By, Bz);
+    debug_them!(Ca, Cc, Cy, Cz);
+    debug_them!(Da, Dc, Dy, Dz);
+    // macro that prints each variable to std_err to ensure compiler doesn't "optimize" away the computation
+}
+
 
 fn main() {
     let num_threads = 16;
@@ -66,9 +203,65 @@ fn main() {
     // runs the function `do_math()` 16 times on 16 threads
     rayon::scope(|s| {
         for _ in 0..num_threads {
-            s.spawn(|_| do_math())
+            s.spawn(|_| do_math_1())
         };
     });
 
-    println!("In all, Took {:?} seconds. {:?} GIOPS.", now.elapsed().as_secs_f32(), (25 * 10 * num_threads) as f32 / now.elapsed().as_secs_f32());
+    println!("In all, Took {:?} seconds. {:?} GIOPS.", now.elapsed().as_secs_f32(), (25 * num_threads * 2) as f32 / now.elapsed().as_secs_f32());
+
+    let now = Instant::now();    
+
+    // runs the function `do_math()` 16 times on 16 threads
+    rayon::scope(|s| {
+        for _ in 0..num_threads {
+            s.spawn(|_| do_math_1())
+        };
+    });
+
+    println!("In all, Took {:?} seconds. {:?} GIOPS.", now.elapsed().as_secs_f32(), (25 * num_threads * 1) as f32 / now.elapsed().as_secs_f32());
+
+    let now = Instant::now();    
+
+    // runs the function `do_math()` 16 times on 16 threads
+    rayon::scope(|s| {
+        for _ in 0..num_threads {
+            s.spawn(|_| do_math_2())
+        };
+    });
+
+    println!("In all, Took {:?} seconds. {:?} GIOPS.", now.elapsed().as_secs_f32(), (25 * num_threads * 2) as f32 / now.elapsed().as_secs_f32());
+
+    let now = Instant::now();    
+
+    // runs the function `do_math()` 16 times on 16 threads
+    rayon::scope(|s| {
+        for _ in 0..num_threads {
+            s.spawn(|_| do_math_3())
+        };
+    });
+
+    println!("In all, Took {:?} seconds. {:?} GIOPS.", now.elapsed().as_secs_f32(), (25 * num_threads * 3) as f32 / now.elapsed().as_secs_f32());
+
+    let now = Instant::now();    
+
+    // runs the function `do_math()` 16 times on 16 threads
+    rayon::scope(|s| {
+        for _ in 0..num_threads {
+            s.spawn(|_| do_math_4())
+        };
+    });
+
+    println!("In all, Took {:?} seconds. {:?} GIOPS.", now.elapsed().as_secs_f32(), (25 * num_threads * 4) as f32 / now.elapsed().as_secs_f32());
+
+    let now = Instant::now();    
+
+    // runs the function `do_math()` 16 times on 16 threads
+    rayon::scope(|s| {
+        for _ in 0..num_threads {
+            s.spawn(|_| do_math_5())
+        };
+    });
+
+    println!("In all, Took {:?} seconds. {:?} GIOPS.", now.elapsed().as_secs_f32(), (25 * num_threads * 5) as f32 / now.elapsed().as_secs_f32());
+
 }
