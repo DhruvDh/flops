dc01ff525e666e076d4a18457f4902a611933fdd
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2019-09-11 21:39:53 -0400

    get me 1.2 tflops

--- src/main.rs
+++ src/main.rs
@@ -28,120 +28,58 @@ macro_rules! create_variables {
 }
 
 macro_rules! mul_add_them {
-    ($($x:ident, $y:ident, $z:ident),+) => {
+    ($(($x:ident, $y:ident, $z:ident)),+) => {
         $(
             $x = $x.mul_add($y, $z);
         )+
     };
 }
 
-fn do_stuff() -> f32 {
-    create_variables!(a, b, c, x, y, z, i, j, k, m, n, o,, new_simd_float);
-    create_variables!(A, B, C, D, X, Y, Z, I, J, K, M, N, O,, new_simd_float);
-    // macro that expands to let a = new_simd_float() for each variable a, b, c...
+macro_rules! debug_them {
+    ($($x:ident),+) => {
+        $(
+            dbg!(&$x);
+        )+
+    }
+}
 
-    let now = Instant::now();
+fn do_math() {
+    create_variables!(a, b, c, x, y, z, i, j, k, m, n, o, d, e, f,, new_simd_float);
+    // macro that expands to let a = new_simd_float(); for each variable a, b, c...
+    create_variables!(A, B, C, X, Y, Z, I, J, K, M, N, O, D, E, F,, new_simd_float);
+    // basically initializes each variable to an array of random 8 32-bit floats that use SIMD operations 
+
     for _ in 0..100_000_000 {
         for _ in 0..50 {
-            mul_add_them!(a, b, c,  x, y, z, i, j, k, m, n, o);
-            mul_add_them!(A, B, C, X, Y, Z, I, J, K, M, N,  O);
+            mul_add_them!((a, b, c),  (x, y, z), (i, j, k), (m, n, o), (d, e, f));
             // a macro that expands to a = a.mul_add(b, c); x = x.mul_add(y, z) and so on for each variable.
+            // basically performs fused multiply add 5 times, once for each trio of variables.
+            mul_add_them!((A, B, C), (X, Y, Z), (I, J, K), (M, N, O), (D, E, F));
+            // 10 fused multiply adds on arrays of 8 32-bit floats for a total of
+            // 10 * 8 * 2 = 160 floating point operations in one iteration
         }
     }
     
-    dbg!(&a); // reading the value to ensure compiler actually computes them
-    dbg!(&x); // reading the value to ensure compiler actually computes them
-    dbg!(&i); // reading the value to ensure compiler actually computes them
-    dbg!(&m); // reading the value to ensure compiler actually computes them
-
-    dbg!(&A); // reading the value to ensure compiler actually computes them
-    dbg!(&X); // reading the value to ensure compiler actually computes them
-    dbg!(&I); // reading the value to ensure compiler actually computes them
-    dbg!(&M); // reading the value to ensure compiler actually computes them
-
-    
-
-    now.elapsed().as_secs_f32()
+    debug_them!(a, x, i, m, d);
+    debug_them!(A, X, I, M, D);    
+    // macro that prints each variable to std_err to ensure compiler doesn't "optimize" away the computation
 }
 
 fn main() {
-    let threads = 1;
-    rayon::ThreadPoolBuilder::new().num_threads(32).build_global().unwrap();
+    let num_threads = 16;
+    rayon::ThreadPoolBuilder::new().num_threads(num_threads).build_global().unwrap();
+    // builds a threadpool of 16 threads
     
+    // a time instant used for benchmarking
     let now = Instant::now();    
+
+
+    // runs the function `do_math()` 16 times on 16 threads
     rayon::scope(|s| {
-        for _ in 0..threads {
-            s.spawn(|_| {
-                println!("Took {:?} seconds.", do_stuff());
-            })
+        for _ in 0..num_threads {
+            s.spawn(|_| do_math())
         };
     });
 
-    println!("\n\nIn all, Took {:?} seconds for {} threads.", now.elapsed().as_secs_f32(), threads);
-    let threads = 2;
-    
-    let now = Instant::now();    
-    rayon::scope(|s| {
-        for _ in 0..threads {
-            s.spawn(|_| {
-                println!("Took {:?} seconds.", do_stuff());
-            })
-        };
-    });
-
-    println!("\n\nIn all, Took {:?} seconds for {} threads.", now.elapsed().as_secs_f32(), threads);
-    
-
-    let threads = 4;
-    
-    let now = Instant::now();    
-    rayon::scope(|s| {
-        for _ in 0..threads {
-            s.spawn(|_| {
-                println!("Took {:?} seconds.", do_stuff());
-            })
-        };
-    });
-
-    println!("\n\nIn all, Took {:?} seconds for {} threads.", now.elapsed().as_secs_f32(), threads);    
-
-    let threads = 8;
-    
-    let now = Instant::now();    
-    rayon::scope(|s| {
-        for _ in 0..threads {
-            s.spawn(|_| {
-                println!("Took {:?} seconds.", do_stuff());
-            })
-        };
-    });
-
-    println!("\n\nIn all, Took {:?} seconds for {} threads.", now.elapsed().as_secs_f32(), threads);
-
-    let threads = 16;
-    
-    let now = Instant::now();    
-    rayon::scope(|s| {
-        for _ in 0..threads {
-            s.spawn(|_| {
-                println!("Took {:?} seconds.", do_stuff());
-            })
-        };
-    });
-
-    println!("\n\nIn all, Took {:?} seconds for {} threads.", now.elapsed().as_secs_f32(), threads);
-
-    let threads = 32;
-    
-    let now = Instant::now();    
-    rayon::scope(|s| {
-        for _ in 0..threads {
-            s.spawn(|_| {
-                println!("Took {:?} seconds.", do_stuff());
-            })
-        };
-    });
-
-    println!("\n\nIn all, Took {:?} seconds for {} threads.", now.elapsed().as_secs_f32(), threads);
-
+    println!("In all, Took {:?} seconds.", now.elapsed().as_secs_f32());
 }
