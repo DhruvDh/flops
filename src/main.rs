e2fb0d1285eeec2644c2652f41e3a9e092a7a812
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2019-09-12 21:47:57 -0400

    final

--- src/main.rs
+++ src/main.rs
@@ -57,7 +57,7 @@ fn do_math() {
     // same marco also initialized to a random i32 if variables are inside () instead of []
 
     
-    for _ in 0..(1e10 as i64) {
+    for _ in 0..(1e9 as i64) {
         math!(a, b, c, x, y, i, z, j);
         math!(Aa, Ab, Ac, Ax, Ay, Ai, Az, Aj);
         math!(Ba, Bb, Bc, Bx, By, Bi, Bz, Bj);
@@ -66,7 +66,9 @@ fn do_math() {
         math!(Ea, Eb, Ec, Ex, Ey, Ei, Ez, Ej);
         math!(Fa, Fb, Fc, Fx, Fy, Fi, Fz, Fj);
         math!(Ga, Gb, Gc, Gx, Gy, Gi, Gz, Gj);  
-        // does 25 integer ops per math!() macro call among all variables  
+        // does 25 integer ops per math!() macro call among all variables 
+        // one vect mul, one vect add, one vect sub, and a single i32 addtion
+        // 8 such calls amounting too 200 integer OPs per iteration 
     }
     
     debug_them!(a, c, y, z);
@@ -94,5 +96,5 @@ fn main() {
         };
     });
 
-    println!("In all, Took {:?} seconds. {:?} GIOPS.", now.elapsed().as_secs_f32(), (250 * num_threads * 8) as f32 / now.elapsed().as_secs_f32());
+    println!("In all, Took {:?} seconds. {:?} GIOPS.", now.elapsed().as_secs_f32(), (25 * num_threads * 8) as f32 / now.elapsed().as_secs_f32());
 }
