05372836a0542b3bd5fa0663fc22182996b6bf6a
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2019-09-12 20:29:36 -0400

    trying to get rid of cache misses

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
-
 use rand::prelude::*;
 use rayon;
 use packed_simd::{i32x8, f32x8};
 use std::time::Instant;
 
-macro_rules! init_variables {
-    ([$($x:ident),+], $rng:ident) => {
+/// returns an array with 8 floats that will use SIMD instructions for operations
+fn new_simd_float() -> f32x8 {
+    let mut rng = rand::thread_rng();
+    let random_floats: Vec<f32> = (0..8).map(|_| rng.gen()).collect();
+    
+    f32x8::from_slice_unaligned(&random_floats[0..8])
+}
+
+macro_rules! create_variables {
+    ($($x:ident,)+, $y:ident) => {
         $(
-            let mut $x = {
-                let random_ints: Vec<i32> = (0..8).map(|_| $rng.gen()).collect();
-                i32x8::from_slice_unaligned(&random_ints[0..8])
-            };
+            let mut $x = $y();
         )+
     };
-    (($($x:ident),+), $rng:ident) => {
+}
+
+macro_rules! mul_add_them {
+    ($(($x:ident, $y:ident, $z:ident)),+) => {
         $(
-            let mut $x: i32 = $rng.gen();
+            $x = $x.mul_add($y, $y);
         )+
     };
 }
@@ -28,62 +35,32 @@ macro_rules! debug_them {
     }
 }
 
-macro_rules! math {
-    ($a:ident, $b:ident, $c:ident, $x:ident, $y:ident, $i:ident, $z:ident, $j:ident) => {
-        $a = $a * $b;
-        $c = $c + $x;
-        $y = $y - $i;
-        $z = $z.wrapping_add($j);
-    };
-}
-
 fn do_math() {
-    let mut rng = rand::thread_rng();
-
-    init_variables!([a, b, c, x, y, i], rng);
-    init_variables!([Aa, Ab, Ac, Ax, Ay, Ai], rng);
-    init_variables!([Ba, Bb, Bc, Bx, By, Bi], rng);
-    init_variables!([Ca, Cb, Cc, Cx, Cy, Ci], rng);
-    init_variables!([Da, Db, Dc, Dx, Dy, Di], rng);
-    init_variables!([Ea, Eb, Ec, Ex, Ey, Ei], rng);
-    init_variables!([Fa, Fb, Fc, Fx, Fy, Fi], rng);
-    init_variables!([Ga, Gb, Gc, Gx, Gy, Gi], rng);
-    // macro that expands to code that initializes each variable to an array of random 8 32-bit floats that use SIMD operations 
-
-
-    init_variables!((z, j, Az, Aj, Bz, Bj), rng);
-    init_variables!((Cz, Cj, Dz, Dj), rng);
-    init_variables!((Ez, Ej, Fz, Fj, Gz, Gj), rng);
-    // same marco also initialized to a random i32 if variables are inside () instead of []
-
+    create_variables!(a, b, c, x, y, z, i, j, k, m, n, o,, new_simd_float);
+    // macro that expands to let a = new_simd_float(); for each variable a, b, c...
+    create_variables!(A, B, C, X, Y, Z, I, J, K, M, N, O,, new_simd_float);
+    // basically initializes each variable to an array of random 8 32-bit floats that use SIMD operations 
     
-    for _ in 0..(1e9 as i64) {
-        math!(a, b, c, x, y, i, z, j);
-        math!(Aa, Ab, Ac, Ax, Ay, Ai, Az, Aj);
-        math!(Ba, Bb, Bc, Bx, By, Bi, Bz, Bj);
-        math!(Ca, Cb, Cc, Cx, Cy, Ci, Cz, Cj);
-        math!(Da, Db, Dc, Dx, Dy, Di, Dz, Dj);
-        math!(Ea, Eb, Ec, Ex, Ey, Ei, Ez, Ej);
-        math!(Fa, Fb, Fc, Fx, Fy, Fi, Fz, Fj);
-        math!(Ga, Gb, Gc, Gx, Gy, Gi, Gz, Gj);    
+    for _ in 0..(1e10 as i64) {
+        mul_add_them!((a, b, c),  (x, y, z), (i, j, k), (m, n, o));
+        // a macro that expands to a = a.mul_add(b, c); x = x.mul_add(y, z) and so on for each variable.
+        // basically performs fused multiply add 5 times, once for each trio of variables.
+        mul_add_them!((A, B, C), (X, Y, Z), (I, J, K), (M, N, O));
+        // 8 fused multiply adds on arrays of 8 32-bit floats for a total of
+        // 8 * 8 * 2 = 128 floating point operations in one iteration
     }
     
-    debug_them!(a, c, y, z);
-    debug_them!(Aa, Ac, Ay, Az);
-    debug_them!(Ba, Bc, By, Bz);
-    debug_them!(Ca, Cc, Cy, Cz);
-    debug_them!(Da, Dc, Dy, Dz);
-    debug_them!(Ea, Ec, Ey, Ez);
-    debug_them!(Fa, Fc, Fy, Fz);
-    debug_them!(Ga, Gc, Gy, Gz);
+    debug_them!(a, x, i, m);
+    debug_them!(A, X, I, M);    
     // macro that prints each variable to std_err to ensure compiler doesn't "optimize" away the computation
 }
 
 fn main() {
-    let num_threads = 8;
+    let num_threads = 16;
     rayon::ThreadPoolBuilder::new().num_threads(num_threads).build_global().unwrap();
     // builds a threadpool of 16 threads
  
+    // a time instant used for benchmarking
     let now = Instant::now();    
 
     // runs the function `do_math()` 16 times on 16 threads
@@ -93,5 +70,5 @@ fn main() {
         };
     });
 
-    println!("In all, Took {:?} seconds. {:?} GIOPS.", now.elapsed().as_secs_f32(), (25 * num_threads * 8) as f32 / now.elapsed().as_secs_f32());
+    println!("In all, Took {:?} seconds. {:?} GFLOPS.", now.elapsed().as_secs_f32(), (128 * 10 * 16 ) as f32 / now.elapsed().as_secs_f32());
 }
